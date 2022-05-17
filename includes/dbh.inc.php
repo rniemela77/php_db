5a3837e5fc6c65f2165980a823eeae177fff1a44
Robert Niemela <rvniemela@hotmail.com>
2022-05-16 18:49:08 -0700

    remove non-class php code

--- includes/dbh.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Settings for xampp
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "cdidb";
-
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
